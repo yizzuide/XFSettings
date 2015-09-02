009dc381b83240ced8e88fd0465dfe37568c007e
付星 <893695651@qq.com>
2015-09-02 11:09:53 +0800

    update Xcode config

---
+++ XFSettingsExample/XFCellColorData.m
@@ -0,0 +1,13 @@
+//
+//  XFCellColorData.m
+//  XFSettingsExample
+//
+//  Created by 付星 on 15/9/1.
+//  Copyright (c) 2015年 Yizzuide. All rights reserved.
+//
+
+#import "XFCellColorData.h"
+
+@implementation XFCellColorData
+
+@end
