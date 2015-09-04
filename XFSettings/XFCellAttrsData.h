c32aa43c60c9c257410f345d9ca3353957fc3b06
yizzuide <893695651@qq.com>
2015-09-05 00:32:06 +0800

    add attrs cell document info

--- XFSettings/XFCellAttrsData.h
+++ XFSettings/XFCellAttrsData.h
@@ -18,11 +18,29 @@
 @property (nonatomic, strong) UIColor *cellBottomLineColor;
 
 // Content
+/**
+ *  标题颜色
+ */
 @property (nonatomic, strong) UIColor *contentTitleTextColor;
+/**
+ *  详细文字颜色
+ */
 @property (nonatomic, strong) UIColor *contentDetailTextColor;
+/**
+ *  辅助文字颜色
+ */
 @property (nonatomic, strong) UIColor *contentInfoTextColor;
+/**
+ *  标题文字大小（其它文字会按个大小自动调整）
+ */
 @property (nonatomic, assign) CGFloat contentTextMaxSize;
+/**
+ *  设置图标大小
+ */
 @property (nonatomic, assign) CGFloat contentIconSize;
+/**
+ *  设置内容间距
+ */
 @property (nonatomic, assign) CGFloat contentEachOtherPadding;
 
 
